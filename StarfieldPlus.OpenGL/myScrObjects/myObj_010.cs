32cf84139ff9afa82a071f5ae56ac24354c7f9d1
pm <pm@CM33L63.com>
2022-05-31 11:10:29 -0400

    ...

--- StarfieldPlus.OpenGL/myScrObjects/myObj_010.cs
+++ StarfieldPlus.OpenGL/myScrObjects/myObj_010.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 
 /*
-    - Randomly Roaming Squares (Snow Like)
+    - Randomly Roaming Shapes (Snow Like)
 */
 
 
@@ -13,15 +13,17 @@ namespace my
 {
     public class myObj_010 : myObject
     {
-        private int dx, dy, Size, X, Y;
-        float A = 0, R = 0, G = 0, B = 0;
+        private float x, y, dx, dy, Size, angle, dAngle;
+        private float A = 0, R = 0, G = 0, B = 0;
 
-        static bool isDimmable = false, doFillShapes = false;
-        static int minX = 0, minY = 0, maxX = 0, maxY = 0, showMode = 0, maxSize = 0;
-
-        private static int x0, y0, N = 1;
+        private static bool doClearBuffer = false, doFillShapes = false;
+        private static int minX = 0, minY = 0, maxX = 0, maxY = 0, maxSize = 0, N = 1;
+        private static int shapeType = 0, rotationMode = 0, rotationSubMode = 0;
+        private static float dimAlpha = 0.25f;
 
         private static myInstancedPrimitive inst = null;
+
+        // ---------------------------------------------------------------------------------------------------------------
 
         public myObj_010()
         {
@@ -37,14 +39,18 @@ namespace my
                     showMode = rand.Next(2);
                 }
 */
-                isDimmable = rand.Next(2) == 0;
+                doClearBuffer = myUtils.randomBool(rand);
+                doFillShapes  = myUtils.randomBool(rand);
 
-                var alpha = isDimmable ? 33 + rand.Next(130) : 255;
+                // rotationMode: 0, 1 = rotation; 2 = no rotation, angle is 0; 3 = no rotation, angle is not 0
+                rotationMode = rand.Next(4);
+                shapeType = rand.Next(5);
 
-                // Sometimes set dimBrush to very low alpha -- to have longer tails
-                if (isDimmable && rand.Next(11) == 0)
+                // In case the rotation is enabled, we also may enable additional rotation option:
+                if (rotationMode < 2)
                 {
-                    alpha = 7 + rand.Next(11);
+                    rotationSubMode = rand.Next(7);
+                    rotationSubMode = rotationSubMode > 2 ? 0 : rotationSubMode + 1;     // [0, 1, 2] --> [1, 2, 3]; otherwise set to '0';
                 }
 
                 // In case the border is wider than the screen's bounds, the movement looks a bit different (no bouncing)
@@ -55,46 +61,80 @@ namespace my
                 maxX = gl_Width  + offset;
                 maxY = gl_Height + offset;
 
-                maxSize = showMode == 0 ? 11 : 50;
+                maxSize = myUtils.randomChance(rand, 0, 10) ? 33 : 11;
             }
 
-            X = rand.Next(gl_Width);
-            Y = rand.Next(gl_Height);
+            generateNew();
+        }
 
-            int maxSpeed = 20;
+        // ---------------------------------------------------------------------------------------------------------------
 
-            dx = (rand.Next(maxSpeed) + 1) * (rand.Next(2) == 0 ? 1 : -1);
-            dy = (rand.Next(maxSpeed) + 1) * (rand.Next(2) == 0 ? 1 : -1);
+        protected override string CollectCurrentInfo()
+        {
+/*
+            string str = $"Obj = myObj_010\n\n" +
+                            $"N = {N}\n" +
+                            $"renderDelay = {renderDelay}\n" +
+                            $"moveType = {moveType}\n" +
+                            $"shapeType = {shapeType}\n" +
+                            $"rotationMode = {rotationMode}\n" +
+                            $"rotationSubMode = {rotationSubMode}\n" +
+                            $"colorMode = {colorMode}";
+*/
+
+            string str = $"Obj = myObj_010\n\n" +
+                            $"N = {N}\n" +
+                            $"renderDelay = {renderDelay}" +
+                            $"shapeType = {shapeType}\n" + 
+                            $"rotationMode = {rotationMode}" +
+                            $"rotationSubMode = {rotationSubMode}";
+            return str;
+        }
+
+        // ---------------------------------------------------------------------------------------------------------------
+
+        protected override void generateNew()
+        {
+            x = rand.Next(gl_Width);
+            y = rand.Next(gl_Height);
+
+            int maxSpeed = 2000;
+
+            dx = 0.01f * (rand.Next(maxSpeed) + 1) * myUtils.randomSign(rand);
+            dy = 0.01f * (rand.Next(maxSpeed) + 1) * myUtils.randomSign(rand);
 
             Size = rand.Next(maxSize) + 1;
 
-            //A = rand.Next(256 - 75) + 75;
-            //colorPicker.getColor(X, Y, ref R, ref G, ref B);
-
             A = (float)rand.NextDouble();
-            R = (float)rand.NextDouble();
-            G = (float)rand.NextDouble();
-            B = (float)rand.NextDouble();
 
-            A = 0.50f;
-            R = 0.75f;
-            G = 0.50f;
-            B = 0.50f;
+            colorPicker.getColor(x, y, ref R, ref G, ref B);
+
+            angle = 0;
+            dAngle = rotationMode < 2 ? 0.001f * rand.Next(111) * myUtils.randomSign(rand) : 0;
+
+            // There will be no rotation, but the angle is set to non-zero
+            if (rotationMode == 3)
+            {
+                angle = (float)rand.NextDouble() * 111;
+            }
+
+            return;
         }
 
-        // -------------------------------------------------------------------------
+        // ---------------------------------------------------------------------------------------------------------------
 
         protected override void Move()
         {
-            X += dx;
-            Y += dy;
+            x += dx;
+            y += dy;
+            angle += dAngle;
 
-            if (X < minX || X > maxX)
+            if (x < minX || x > maxX)
             {
                 dx *= -1;
             }
 
-            if (Y < minY || Y > maxY)
+            if (y < minY || y > maxY)
             {
                 dy *= -1;
             }
@@ -102,63 +142,103 @@ namespace my
             return;
         }
 
-        // -------------------------------------------------------------------------
+        // ---------------------------------------------------------------------------------------------------------------
 
         protected override void Show()
         {
-            switch (showMode)
+            switch (shapeType)
             {
                 case 0:
                     var rectInst = inst as myRectangleInst;
 
-                    rectInst.setInstanceCoords(X, Y, 2 * Size, 2 * Size);
+                    rectInst.setInstanceCoords(x - Size, y - Size, 2 * Size, 2 * Size);
                     rectInst.setInstanceColor(R, G, B, A);
-                    rectInst.setInstanceAngle(0);
+                    rectInst.setInstanceAngle(angle);
                     break;
 
+                case 1:
+                    var triangleInst = inst as myTriangleInst;
 
-
-                // Solid color
-/*
-                case 0:
-                    br.Color = Color.FromArgb(A, R, G, B);
-                    g.FillRectangle(br, X, Y, Size, Size);
-
-                    if (Size > 3)
-                    {
-                        g.FillRectangle(Brushes.Black, X, Y, 1, 1);
-                        g.FillRectangle(Brushes.Black, X + Size - 1, Y, 1, 1);
-                        g.FillRectangle(Brushes.Black, X, Y + Size - 1, 1, 1);
-                        g.FillRectangle(Brushes.Black, X + Size - 1, Y + Size - 1, 1, 1);
-                    }
+                    triangleInst.setInstanceCoords(x, y, Size, angle);
+                    triangleInst.setInstanceColor(R, G, B, A);
                     break;
-*/
+
+                case 2:
+                    var ellipseInst = inst as myEllipseInst;
+
+                    ellipseInst.setInstanceCoords(x, y, 2 * Size, angle);
+                    ellipseInst.setInstanceColor(R, G, B, A);
+                    break;
+
+                case 3:
+                    var pentagonInst = inst as myPentagonInst;
+
+                    pentagonInst.setInstanceCoords(x, y, 2 * Size, angle);
+                    pentagonInst.setInstanceColor(R, G, B, A);
+                    break;
+
+                case 4:
+                    var hexagonInst = inst as myHexagonInst;
+
+                    hexagonInst.setInstanceCoords(x, y, 2 * Size, angle);
+                    hexagonInst.setInstanceColor(R, G, B, A);
+                    break;
             }
 
             return;
         }
 
-        // -------------------------------------------------------------------------
+        // ---------------------------------------------------------------------------------------------------------------
 
         protected override void Process(Window window)
         {
-            N = 10;
-            renderDelay = 1;
+            uint cnt = 0;
+
+            N = 500;
+            renderDelay = 10;
+
 
             myPrimitive.init_Rectangle();
 
-            myPrimitive.init_RectangleInst(N);
-            myPrimitive._RectangleInst.setRotationMode(0);
-            inst = myPrimitive._RectangleInst;
 
-
-            while (list.Count < N)
+            switch (shapeType)
             {
-                list.Add(new myObj_010());
+                case 0:
+                    myPrimitive.init_RectangleInst(N);
+                    myPrimitive._RectangleInst.setRotationMode(rotationSubMode);
+                    inst = myPrimitive._RectangleInst;
+                    break;
+
+                case 1:
+                    myPrimitive.init_TriangleInst(N);
+                    myPrimitive._TriangleInst.setRotationMode(rotationSubMode);
+                    inst = myPrimitive._TriangleInst;
+                    break;
+
+                case 2:
+                    myPrimitive.init_EllipseInst(N);
+                    myPrimitive._EllipseInst.setRotationMode(rotationSubMode);
+                    inst = myPrimitive._EllipseInst;
+                    break;
+
+                case 3:
+                    myPrimitive.init_PentagonInst(N);
+                    myPrimitive._PentagonInst.setRotationMode(rotationSubMode);
+                    inst = myPrimitive._PentagonInst;
+                    break;
+
+                case 4:
+                    myPrimitive.init_HexagonInst(N);
+                    myPrimitive._HexagonInst.setRotationMode(rotationSubMode);
+                    inst = myPrimitive._HexagonInst;
+                    break;
             }
 
 
-            glDrawBuffer(GL_FRONT_AND_BACK);
+            if (doClearBuffer == false)
+            {
+                glDrawBuffer(GL_FRONT_AND_BACK);
+            }
 
 
             while (!Glfw.WindowShouldClose(window))
@@ -169,8 +249,19 @@ namespace my
                 Glfw.SwapBuffers(window);
                 Glfw.PollEvents();
 
-                glClearColor(0, 0, 0, 1);
-                glClear(GL_COLOR_BUFFER_BIT);
+                // Dim the screen constantly
+                if (doClearBuffer == false)
+                {
+                    myPrimitive._Rectangle.SetAngle(0);
+                    // Shift background color just a bit, to hide long lasting traces of shapes
+                    myPrimitive._Rectangle.SetColor(rand.Next(5) * 0.01f, rand.Next(5) * 0.01f, rand.Next(5) * 0.01f, dimAlpha);
+                    myPrimitive._Rectangle.Draw(0, 0, gl_Width, gl_Height, true);
+                }
+                else
+                {
+                    glClearColor(0, 0, 0, 1);
+                    glClear(GL_COLOR_BUFFER_BIT);
+                }
 
                 inst.ResetBuffer();
 
@@ -178,7 +269,7 @@ namespace my
                 {
                     var obj = list[i] as myObj_010;
                     obj.Show();
-                    //obj.Move();
+                    obj.Move();
                 }
 
                 if (doFillShapes)
@@ -192,44 +283,19 @@ namespace my
                 inst.SetColorA(0);
                 inst.Draw(false);
 
+                if (list.Count < N)
+                {
+                    list.Add(new myObj_010());
+                }
+
                 System.Threading.Thread.Sleep(renderDelay);
+
+                cnt++;
             }
 
-
-/*
-            g.FillRectangle(Brushes.Black, 0, 0, Width, Height);
-
-            while (isAlive && list.Count < Count)
-            {
-                g.FillRectangle(dimBrush, 0, 0, Width, Height);
-
-                foreach (myObj_010 s in list)
-                {
-                    s.Show();
-                    s.Move();
-                }
-
-                list.Add(new myObj_010());
-
-                form.Invalidate();
-                System.Threading.Thread.Sleep(t);
-            }
-
-            while (isAlive)
-            {
-                g.FillRectangle(dimBrush, 0, 0, Width, Height);
-
-                foreach (myObj_010 s in list)
-                {
-                    s.Show();
-                    s.Move();
-                }
-
-                form.Invalidate();
-                System.Threading.Thread.Sleep(t);
-            }
-*/
             return;
         }
+
+        // ---------------------------------------------------------------------------------------------------------------
     };
 };
