6c9cfb03645b3f4d0f3bcf834cdbda492f2cccb9
pm <pm@CM33L63.com>
2023-02-02 12:10:26 -0500

    ...

--- StarfieldPlus.OpenGL/myScrObjects/myObj_110.cs
+++ StarfieldPlus.OpenGL/myScrObjects/myObj_110.cs
@@ -13,12 +13,13 @@ namespace my
 {
     public class myObj_110 : myObject
     {
-        private int x, y, w, h, size;
+        private int x, y, w, h, size, cnt;
         private float A, R, G, B, angle = 0;
 
-        private static int N = 0, shape = 0, borderMode = 0, borderOpacityMode = 0, shapeOpacityMode = 0, angleMode = 0, maxSize = 0;
-        private static bool doUseRandomSize = false, doUseWasH = false;
-        private static float dimAlpha = 0.05f, brdrR = 0, brdrG = 0, brdrB = 0;
+        private static int N = 0, shape = 0, borderMode = 0, borderOpacityMode = 0, shapeOpacityMode = 0, angleMode = 0,
+                           maxSize = 0, maxCnt = 0;
+        private static bool isOpenGlReady = false, doUseRandomSize = false, doUseWasH = false;
+        private static float lineWidth = 0, dimAlpha = 0.05f, brdrR = 0, brdrG = 0, brdrB = 0;
 
         // ---------------------------------------------------------------------------------------------------------------
 
@@ -51,7 +52,7 @@ namespace my
             shape = rand.Next(6);                                       // Extra one for a random shape selection
 
             doClearBuffer = myUtils.randomChance(rand, 1, 5);
-            dimAlpha = myUtils.randFloat(rand) * 0.05f;
+            dimAlpha = myUtils.randFloat(rand) * 0.01f * N;
 
             doUseRandomSize = myUtils.randomChance(rand, 1, 2);
             doUseWasH       = myUtils.randomChance(rand, 1, 2);         // Whether h == w
@@ -59,14 +60,17 @@ namespace my
             borderMode = rand.Next(8);
             borderOpacityMode = rand.Next(6);
             shapeOpacityMode = rand.Next(10);
-            angleMode = rand.Next(5);
+            angleMode = rand.Next(7);
 
             // Custom border color
             brdrR = myUtils.randFloat(rand);
             brdrG = myUtils.randFloat(rand);
             brdrB = myUtils.randFloat(rand);
 
-            switch (rand.Next(11))
+            // Border line width
+            lineWidth = 0.25f + myUtils.randFloat(rand) * rand.Next(3);
+
+            switch (rand.Next(21))
             {
                 case 0:
                     maxSize = rand.Next(666) + 1;
@@ -80,15 +84,20 @@ namespace my
                     maxSize = rand.Next(333) + 50;
                     break;
 
-                case 6: case 7: case 8:
+                case 6: case 7: case 8: case 9: case 10:
                     maxSize = rand.Next(222) + 75;
                     break;
 
-                case 9: case 10:
+                case 11: case 12: case 13: case 14: case 15:
                     maxSize = rand.Next(111) + 100;
+                    break;
+
+                case 16: case 17: case 18: case 19: case 20:
+                    maxSize = rand.Next(50) + 50;
                     break;
             }
 
+            maxCnt = rand.Next(N * 3) + N;
             renderDelay = rand.Next(20);
 
 #if false
@@ -121,7 +130,9 @@ namespace my
                             $"shapeOpacityMode = {shapeOpacityMode}\n"   +
                             $"angleMode = {angleMode}\n"                 +
                             $"dimAlpha = {fStr(dimAlpha)}\n"             +
+                            $"lineWidth = {fStr(lineWidth)}\n"           +
                             $"renderDelay = {renderDelay}\n"             +
+                            $"maxCnt = {maxCnt}\n"                       +
                             $"file: {colorPicker.GetFileName()}\n"       +
                             $"colorMode = {colorPicker.getModeStr()}"
                 ;
@@ -144,17 +155,25 @@ namespace my
 
         protected override void generateNew()
         {
+            // Overall speed factor
+            cnt = rand.Next(maxCnt);
+
             if (doUseRandomSize)
             {
                 size = rand.Next(maxSize) + 3;
             }
             else
             {
-                size = maxSize > 333 ? maxSize/2 : maxSize;
+                size = maxSize > 333 ? maxSize/3 : maxSize;
             }
 
-            x = rand.Next(gl_Width)  - size;
-            y = rand.Next(gl_Height) - size;
+            x = rand.Next(gl_Width);
+            y = rand.Next(gl_Height);
+
+            colorPicker.getColor(x, y, ref R, ref G, ref B);
+
+            x -= size;
+            y -= size;
 
             if (doUseWasH)
             {
@@ -166,8 +185,6 @@ namespace my
                 w = size * 2;
                 h = size * 1;
             }
-
-            colorPicker.getColor(x, y, ref R, ref G, ref B);
 
             // Adjust the color in case colorPicker returns const color
             if (colorPicker.getMode() == (int)myColorPicker.colorMode.SINGLE_RANDOM)
@@ -230,24 +247,24 @@ namespace my
 
             switch (angleMode)
             {
-                case 0:
+                case 0: case 1: case 2:
                     angle = 0;
                     break;
 
-                case 1:
-                    angle = myUtils.randFloat(rand) * 0.10f;
-                    break;
-
-                case 2:
-                    angle = myUtils.randFloat(rand) * 0.25f;
-                    break;
-
                 case 3:
-                    angle = myUtils.randFloat(rand) * 0.50f;
+                    angle = myUtils.randomSign(rand) * myUtils.randFloat(rand) * 0.10f;
                     break;
 
                 case 4:
-                    angle = myUtils.randFloat(rand);
+                    angle = myUtils.randomSign(rand) * myUtils.randFloat(rand) * 0.25f;
+                    break;
+
+                case 5:
+                    angle = myUtils.randomSign(rand) * myUtils.randFloat(rand) * 0.50f;
+                    break;
+
+                case 6:
+                    angle = myUtils.randomSign(rand) * myUtils.randFloat(rand);
                     break;
             }
 
@@ -258,7 +275,12 @@ namespace my
 
         protected override void Move()
         {
-            generateNew();
+            if (cnt > 0)
+                cnt--;
+            else
+            {
+                generateNew();
+            }
 
             return;
         }
@@ -267,166 +289,167 @@ namespace my
 
         protected override void Show()
         {
-            float size2x = size * 2;
-
-            float bR = 0, bG = 0, bB = 0, bA = 0;
-
-            switch (borderMode)
+            if (cnt == 0)
             {
-                // No border
-                case 0:
-                    break;
+                float bR = 0, bG = 0, bB = 0, bA = 0;
 
-                // The same color
-                case 1:
-                    bR = R;
-                    bG = G;
-                    bB = B;
-                    break;
+                switch (borderMode)
+                {
+                    // No border
+                    case 0:
+                        break;
 
-                // Black
-                case 2:
-                    bR = 0;
-                    bG = 0;
-                    bB = 0;
-                    break;
+                    // The same color
+                    case 1:
+                        bR = R;
+                        bG = G;
+                        bB = B;
+                        break;
 
-                // Dark random
-                case 3:
-                    bR = myUtils.randFloat(rand) * 0.1f;
-                    bG = myUtils.randFloat(rand) * 0.1f;
-                    bB = myUtils.randFloat(rand) * 0.1f;
-                    break;
+                    // Black
+                    case 2:
+                        bR = 0;
+                        bG = 0;
+                        bB = 0;
+                        break;
 
-                // White
-                case 4:
-                    bR = 1;
-                    bG = 1;
-                    bB = 1;
-                    break;
+                    // Dark random
+                    case 3:
+                        bR = myUtils.randFloat(rand) * 0.1f;
+                        bG = myUtils.randFloat(rand) * 0.1f;
+                        bB = myUtils.randFloat(rand) * 0.1f;
+                        break;
 
-                // Light random
-                case 5:
-                    bR = 1.0f - myUtils.randFloat(rand) * 0.1f;
-                    bG = 1.0f - myUtils.randFloat(rand) * 0.1f;
-                    bB = 1.0f - myUtils.randFloat(rand) * 0.1f;
-                    break;
+                    // White
+                    case 4:
+                        bR = 1;
+                        bG = 1;
+                        bB = 1;
+                        break;
 
-                // Custom color
-                case 6:
-                    bR = brdrR;
-                    bG = brdrG;
-                    bB = brdrB;
-                    break;
+                    // Light random
+                    case 5:
+                        bR = 1.0f - myUtils.randFloat(rand) * 0.1f;
+                        bG = 1.0f - myUtils.randFloat(rand) * 0.1f;
+                        bB = 1.0f - myUtils.randFloat(rand) * 0.1f;
+                        break;
 
-                // Random color
-                case 7:
-                    bR = myUtils.randFloat(rand);
-                    bG = myUtils.randFloat(rand);
-                    bB = myUtils.randFloat(rand);
-                    break;
-            }
+                    // Custom color
+                    case 6:
+                        bR = brdrR;
+                        bG = brdrG;
+                        bB = brdrB;
+                        break;
 
-            switch (borderOpacityMode)
-            {
-                case 0:
-                    bA = A;
-                    break;
+                    // Random color
+                    case 7:
+                        bR = myUtils.randFloat(rand);
+                        bG = myUtils.randFloat(rand);
+                        bB = myUtils.randFloat(rand);
+                        break;
+                }
 
-                case 1:
-                    bA = A * 2;
-                    break;
+                switch (borderOpacityMode)
+                {
+                    case 0:
+                        bA = A;
+                        break;
 
-                case 2:
-                    bA = A * 3;
-                    break;
+                    case 1:
+                        bA = A * 2;
+                        break;
 
-                case 3:
-                    bA = 0.5f;
-                    break;
+                    case 2:
+                        bA = A * 3;
+                        break;
 
-                case 4:
-                    bA = 1.0f;
-                    break;
+                    case 3:
+                        bA = 0.5f;
+                        break;
 
-                case 5:
-                    bA = myUtils.randFloat(rand, 0.1f);
-                    break;
-            }
+                    case 4:
+                        bA = 1.0f;
+                        break;
 
-            int Shape = (shape < 5) ? shape : rand.Next(5);
+                    case 5:
+                        bA = myUtils.randFloat(rand, 0.1f);
+                        break;
+                }
 
-            switch (Shape)
-            {
-                // Rectangle
-                case 0:
-                    myPrimitive._Rectangle.SetAngle(angle);
-                    myPrimitive._Rectangle.SetColor(R, G, B, A);
-                    myPrimitive._Rectangle.Draw(x, y, w, h, true);
+                int Shape = (shape < 5) ? shape : rand.Next(5);
 
-                    myPrimitive._Rectangle.SetColor(bR, bG, bB, bA);
-                    myPrimitive._Rectangle.Draw(x, y, w, h, false);
-                    break;
+                switch (Shape)
+                {
+                    // Rectangle
+                    case 0:
+                        myPrimitive._Rectangle.SetAngle(angle);
+                        myPrimitive._Rectangle.SetColor(R, G, B, A);
+                        myPrimitive._Rectangle.Draw(x, y, w, h, true);
 
-                // Triangle
-                case 1:
-                    myPrimitive._Triangle.SetAngle(angle);
+                        myPrimitive._Rectangle.SetColor(bR, bG, bB, bA);
+                        myPrimitive._Rectangle.Draw(x, y, w, h, false);
+                        break;
 
-                    if (w == h)
-                    {
-                        myPrimitive._Triangle.SetColor(R, G, B, A);
-                        myPrimitive._Triangle.Draw(x, y - w, x - 5 * w / 6, y + w / 2, x + 5 * w / 6, y + w / 2, true);
+                    // Triangle
+                    case 1:
+                        myPrimitive._Triangle.SetAngle(angle);
 
-                        myPrimitive._Triangle.SetColor(bR, bG, bB, bA);
-                        myPrimitive._Triangle.Draw(x, y - w, x - 5 * w / 6, y + w / 2, x + 5 * w / 6, y + w / 2, false);
-                    }
-                    else
-                    {
-                        int x1 = x + size + myUtils.randomSign(rand) * rand.Next(size/2);
-                        int y1 = y + rand.Next(size/2);
+                        if (w == h)
+                        {
+                            myPrimitive._Triangle.SetColor(R, G, B, A);
+                            myPrimitive._Triangle.Draw(x, y - w, x - 5 * w / 6, y + w / 2, x + 5 * w / 6, y + w / 2, true);
 
-                        int x2 = x + myUtils.randomSign(rand) * rand.Next(size/2);
-                        int y2 = y + w + myUtils.randomSign(rand) * rand.Next(size/2);
+                            myPrimitive._Triangle.SetColor(bR, bG, bB, bA);
+                            myPrimitive._Triangle.Draw(x, y - w, x - 5 * w / 6, y + w / 2, x + 5 * w / 6, y + w / 2, false);
+                        }
+                        else
+                        {
+                            int x1 = x + size + myUtils.randomSign(rand) * rand.Next(size / 2);
+                            int y1 = y + rand.Next(size / 2);
 
-                        int x3 = x + w + myUtils.randomSign(rand) * rand.Next(size/2);
-                        int y3 = y + w + myUtils.randomSign(rand) * rand.Next(size/2);
+                            int x2 = x + myUtils.randomSign(rand) * rand.Next(size / 2);
+                            int y2 = y + w + myUtils.randomSign(rand) * rand.Next(size / 2);
 
-                        myPrimitive._Triangle.SetColor(R, G, B, A);
-                        myPrimitive._Triangle.Draw(x1, y1, x2, y2, x3, y3, true);
+                            int x3 = x + w + myUtils.randomSign(rand) * rand.Next(size / 2);
+                            int y3 = y + w + myUtils.randomSign(rand) * rand.Next(size / 2);
 
-                        myPrimitive._Triangle.SetColor(bR, bG, bB, bA);
-                        myPrimitive._Triangle.Draw(x1, y1, x2, y2, x3, y3, false);
-                    }
-                    break;
+                            myPrimitive._Triangle.SetColor(R, G, B, A);
+                            myPrimitive._Triangle.Draw(x1, y1, x2, y2, x3, y3, true);
 
-                // Circle
-                case 2:
-                    myPrimitive._Ellipse.SetColor(R, G, B, A);
-                    myPrimitive._Ellipse.Draw(x, y, w, h, true);
+                            myPrimitive._Triangle.SetColor(bR, bG, bB, bA);
+                            myPrimitive._Triangle.Draw(x1, y1, x2, y2, x3, y3, false);
+                        }
+                        break;
 
-                    myPrimitive._Ellipse.SetColor(bR, bG, bB, bA);
-                    myPrimitive._Ellipse.Draw(x, y, w, h, false);
-                    break;
+                    // Circle
+                    case 2:
+                        myPrimitive._Ellipse.SetColor(R, G, B, A);
+                        myPrimitive._Ellipse.Draw(x, y, w, h, true);
 
-                // Pentagon
-                case 3:
-                    myPrimitive._Pentagon.SetAngle(angle);
-                    myPrimitive._Pentagon.SetColor(R, G, B, A);
-                    myPrimitive._Pentagon.Draw(x, y, w, true);
+                        myPrimitive._Ellipse.SetColor(bR, bG, bB, bA);
+                        myPrimitive._Ellipse.Draw(x, y, w, h, false);
+                        break;
 
-                    myPrimitive._Pentagon.SetColor(bR, bG, bB, bA);
-                    myPrimitive._Pentagon.Draw(x, y, w, false);
-                    break;
+                    // Pentagon
+                    case 3:
+                        myPrimitive._Pentagon.SetAngle(angle);
+                        myPrimitive._Pentagon.SetColor(R, G, B, A);
+                        myPrimitive._Pentagon.Draw(x, y, w, true);
 
-                // Hexagon
-                case 4:
-                    myPrimitive._Hexagon.SetAngle(angle);
-                    myPrimitive._Hexagon.SetColor(R, G, B, A);
-                    myPrimitive._Hexagon.Draw(x, y, w, true);
+                        myPrimitive._Pentagon.SetColor(bR, bG, bB, bA);
+                        myPrimitive._Pentagon.Draw(x, y, w, false);
+                        break;
 
-                    myPrimitive._Hexagon.SetColor(bR, bG, bB, bA);
-                    myPrimitive._Hexagon.Draw(x, y, w, false);
-                    break;
+                    // Hexagon
+                    case 4:
+                        myPrimitive._Hexagon.SetAngle(angle);
+                        myPrimitive._Hexagon.SetColor(R, G, B, A);
+                        myPrimitive._Hexagon.Draw(x, y, w, true);
+
+                        myPrimitive._Hexagon.SetColor(bR, bG, bB, bA);
+                        myPrimitive._Hexagon.Draw(x, y, w, false);
+                        break;
+                }
             }
 
             return;
@@ -462,6 +485,8 @@ namespace my
                 // Swap fore/back framebuffers, and poll for operating system events.
                 Glfw.SwapBuffers(window);
                 Glfw.PollEvents();
+
+                glLineWidth(lineWidth);
 
                 // Dim screen
                 {
@@ -505,6 +530,8 @@ namespace my
             myPrimitive.init_Ellipse();
             myPrimitive.init_Pentagon();
             myPrimitive.init_Hexagon();
+
+            isOpenGlReady = true;
 
             return;
         }
