80ccc485af235b50d2a2dfcc5e7bcf30d5a99d64
PoorLissa <poortwistme@gmail.com>
2023-02-26 09:26:24 -0500

    ...

--- StarfieldPlus.OpenGL/ScreenSaver.cs
+++ StarfieldPlus.OpenGL/ScreenSaver.cs
@@ -109,7 +109,7 @@ public class ScreenSaver
         ids id = (ids)(new System.Random()).Next((int)ids.myObj_last);
 
 #if DEBUG
-        id = ids.myObj_041;
+        id = ids.myObj_330;
 #endif
 
         switch (id)
