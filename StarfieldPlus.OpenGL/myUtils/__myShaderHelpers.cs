b2ebb28c9670a5962c6f375a1aea3023bcb01a80
pm <pm@CM33L63.com>
2023-03-16 15:20:40 -0400

    ...

--- StarfieldPlus.OpenGL/myUtils/__myShaderHelpers.cs
+++ StarfieldPlus.OpenGL/myUtils/__myShaderHelpers.cs
@@ -40,7 +40,7 @@ namespace my
 
             // Plane - exact (n must be normalized)
             public static string planeSDF = $@"
-                float planeSDF( vec3 p, vec4 n ) {{
+                float planeSDF(vec3 p, vec4 n) {{
                     return dot(p,n.xyz) + n.w;
                 }}";
 
@@ -50,9 +50,7 @@ namespace my
                     const vec3 k = vec3(-0.8660254, 0.5, 0.57735);
                     p = abs(p);
                     p.xy -= 2.0*min(dot(k.xy, p.xy), 0.0)*k.xy;
-                    vec2 d = vec2(
-                        length(p.xy-vec2(clamp(p.x,-k.z*h.x,k.z*h.x), h.x))*sign(p.y-h.x),
-                        p.z-h.y);
+                    vec2 d = vec2(length(p.xy-vec2(clamp(p.x,-k.z*h.x,k.z*h.x), h.x))*sign(p.y-h.x), p.z-h.y);
                     return min(max(d.x,d.y),0.0) + length(max(d,0.0));
                 }}";
         }
