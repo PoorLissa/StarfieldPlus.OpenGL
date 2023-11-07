e283bc010fd8f878d55585af8bf02c43235b953b
pm <pm@CM33L63.com>
2023-11-07 12:14:54 -0500

    ...

--- StarfieldPlus.OpenGL/myScrObjects/myObj_430.cs
+++ StarfieldPlus.OpenGL/myScrObjects/myObj_430.cs
@@ -14,7 +14,7 @@ namespace my
     public class myObj_430 : myObject
     {
         // Priority
-        public static int Priority => 99910;
+        public static int Priority => 10;
 		public static System.Type Type => typeof(myObj_430);
 
         private myObj_430 owner = null;
