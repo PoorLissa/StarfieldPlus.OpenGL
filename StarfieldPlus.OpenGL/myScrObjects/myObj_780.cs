4cd45021d8f143e1401eea9576fa9935a1f2aeb5
pm <pm@CM33L63.com>
2023-11-29 12:20:44 -0500

    ...

--- StarfieldPlus.OpenGL/myScrObjects/myObj_780.cs
+++ StarfieldPlus.OpenGL/myScrObjects/myObj_780.cs
@@ -186,6 +186,8 @@ namespace my
         {
             float size2x = size * 2;
 
+#if true
+
             switch (shape)
             {
                 // Instanced squares
@@ -219,6 +221,54 @@ namespace my
                     myPrimitive._HexagonInst.setInstanceColor(R, G, B, A);
                     break;
             }
+
+#else
+
+            switch (shape)
+            {
+                // Instanced squares
+                case 0:
+                    var rectInst = inst as myRectangleInst;
+
+                    rectInst.setInstanceCoords(x - size, y - size, size2x, size2x);
+                    rectInst.setInstanceColor(R, G, B, A);
+                    rectInst.setInstanceAngle(angle);
+                    break;
+
+                // Instanced triangles
+                case 1:
+                    var triangleInst = inst as myTriangleInst;
+
+                    triangleInst.setInstanceCoords(x, y, size, angle);
+                    triangleInst.setInstanceColor(R, G, B, A);
+                    break;
+
+                // Instanced circles
+                case 2:
+                    var ellipseInst = inst as myEllipseInst;
+
+                    ellipseInst.setInstanceCoords(x, y, size2x, angle);
+                    ellipseInst.setInstanceColor(R, G, B, A);
+                    break;
+
+                // Instanced pentagons
+                case 3:
+                    var pentagonInst = inst as myPentagonInst;
+
+                    pentagonInst.setInstanceCoords(x, y, size2x, angle);
+                    pentagonInst.setInstanceColor(R, G, B, A);
+                    break;
+
+                // Instanced hexagons
+                case 4:
+                    var hexagonInst = inst as myHexagonInst;
+
+                    hexagonInst.setInstanceCoords(x, y, size2x, angle);
+                    hexagonInst.setInstanceColor(R, G, B, A);
+                    break;
+            }
+
+#endif
 
             return;
         }
