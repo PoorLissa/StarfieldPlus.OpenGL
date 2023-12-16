de26f33cd6a2627ef81e5978845902bd5e14da96
poorLissa <poortwistme@gmail.com>
2023-12-16 11:04:19 -0500

    ...

--- StarfieldPlus.OpenGL/myScrObjects/myObj_691.cs
+++ StarfieldPlus.OpenGL/myScrObjects/myObj_691.cs
@@ -14,7 +14,7 @@ namespace my
     public class myObj_691 : myObject
     {
         // Priority
-        public static int Priority => 999910;
+        public static int Priority => 10;
 		public static System.Type Type => typeof(myObj_691);
 
         private int dir;
