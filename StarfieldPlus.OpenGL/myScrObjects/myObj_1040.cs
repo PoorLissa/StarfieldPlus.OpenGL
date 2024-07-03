0cbfc69e60a6e05b41ffdc382afd5cfee927de65
pm <pm@CM33L63.com>
2024-07-03 10:00:06 -0400

    ...

--- StarfieldPlus.OpenGL/myScrObjects/myObj_1040.cs
+++ StarfieldPlus.OpenGL/myScrObjects/myObj_1040.cs
@@ -17,14 +17,12 @@ namespace my
         public static int Priority => 9999910;
 		public static System.Type Type => typeof(myObj_1040);
 
-        private float x, y, dx, dy;
-        private float size, A, R, G, B, angle = 0;
+        private int x, y, cnt;
 
-        private static int N = 0, shape = 0;
-        private static bool doFillShapes = false;
-        private static float dimAlpha = 0.05f;
+        private static int N = 0, cellSize = 1, cellMargin = 0;
 
         private static myScreenGradient grad = null;
+        private static myTexRectangle tex = null;
 
         // ---------------------------------------------------------------------------------------------------------------
 
@@ -39,14 +37,15 @@ namespace my
         // One-time global initialization
         protected override void initGlobal()
         {
-            colorPicker = new myColorPicker(gl_Width, gl_Height);
+            colorPicker = new myColorPicker(gl_Width, gl_Height, mode: myColorPicker.colorMode.IMAGE);
             list = new List<myObject>();
 
             // Global unmutable constants
             {
                 N = rand.Next(10) + 10;
 
-                shape = rand.Next(5);
+                cellSize = 10 + rand.Next(50);
+                cellMargin = 1 + rand.Next(3);
             }
 
             initLocal();
@@ -57,9 +56,9 @@ namespace my
         // One-time local initialization
         private void initLocal()
         {
-            doClearBuffer = myUtils.randomBool(rand);
+            doClearBuffer = false;
 
-            renderDelay = rand.Next(11) + 3;
+            renderDelay = rand.Next(3) + 1;
 
             return;
         }
@@ -72,6 +71,8 @@ namespace my
 
             string str = $"Obj = {Type}\n\n"                  +
                             myUtils.strCountOf(list.Count, N) +
+                            $"cellSize = {cellSize}\n"        +
+                            $"cellMargin = {cellMargin}\n"    +
                             $"renderDelay = {renderDelay}\n"  +
                             $"file: {colorPicker.GetFileName()}"
                 ;
@@ -93,77 +94,29 @@ namespace my
             x = rand.Next(gl_Width);
             y = rand.Next(gl_Height);
 
-            dx = myUtils.randFloat(rand);
-            dy = myUtils.randFloat(rand);
+            x -= x % (cellSize + cellMargin);
+            y -= y % (cellSize + cellMargin);
 
-            size = rand.Next(11) + 3;
-
-            A = 1;
-            R = (float)rand.NextDouble();
-            G = (float)rand.NextDouble();
-            B = (float)rand.NextDouble();
-
-            colorPicker.getColor(x, y, ref R, ref G, ref B);
-
-            return;
+            cnt = 11 + rand.Next(11);
         }
 
         // ---------------------------------------------------------------------------------------------------------------
 
         protected override void Move()
         {
-            x += dx;
-            y += dy;
-
-            if (x < 0 || x > gl_Width || y < 0 || y > gl_Height)
+            if (--cnt == 0)
             {
+                Show();
                 generateNew();
             }
-
-            return;
         }
 
         // ---------------------------------------------------------------------------------------------------------------
 
         protected override void Show()
         {
-            float size2x = size * 2;
-
-            switch (shape)
-            {
-                // Instanced squares
-                case 0:
-                    myPrimitive._RectangleInst.setInstanceCoords(x - size, y - size, size2x, size2x);
-                    myPrimitive._RectangleInst.setInstanceColor(R, G, B, A);
-                    myPrimitive._RectangleInst.setInstanceAngle(angle);
-                    break;
-
-                // Instanced triangles
-                case 1:
-                    myPrimitive._TriangleInst.setInstanceCoords(x, y, size2x, angle);
-                    myPrimitive._TriangleInst.setInstanceColor(R, G, B, A);
-                    break;
-
-                // Instanced circles
-                case 2:
-                    myPrimitive._EllipseInst.setInstanceCoords(x, y, size2x, angle);
-                    myPrimitive._EllipseInst.setInstanceColor(R, G, B, A);
-                    break;
-
-                // Instanced pentagons
-                case 3:
-                    myPrimitive._PentagonInst.setInstanceCoords(x, y, size2x, angle);
-                    myPrimitive._PentagonInst.setInstanceColor(R, G, B, A);
-                    break;
-
-                // Instanced hexagons
-                case 4:
-                    myPrimitive._HexagonInst.setInstanceCoords(x, y, size2x, angle);
-                    myPrimitive._HexagonInst.setInstanceColor(R, G, B, A);
-                    break;
-            }
-
-            return;
+            tex.setOpacity(myUtils.randFloat(rand));
+            tex.Draw(x, y, cellSize, cellSize, x, y, cellSize, cellSize);
         }
 
         // ---------------------------------------------------------------------------------------------------------------
@@ -173,10 +126,9 @@ namespace my
             uint cnt = 0;
             initShapes();
 
-            // Disable VSYNC if needed
-            // Glfw.SwapInterval(0);
+            clearScreenSetup(doClearBuffer, 0.1f, true);
 
-            clearScreenSetup(doClearBuffer, 0.1f);
+            grad.Draw();
 
             while (!Glfw.WindowShouldClose(window))
             {
@@ -188,41 +140,12 @@ namespace my
                 Glfw.SwapBuffers(window);
                 Glfw.PollEvents();
 
-                // Dim screen
-                {
-                    if (doClearBuffer)
-                    {
-                        glClear(GL_COLOR_BUFFER_BIT);
-                        grad.Draw();
-                    }
-                    else
-                    {
-                        dimScreen(dimAlpha);
-                    }
-                }
-
                 // Render Frame
                 {
-                    inst.ResetBuffer();
-
                     for (int i = 0; i != Count; i++)
                     {
-                        var obj = list[i] as myObj_1040;
-
-                        obj.Show();
-                        obj.Move();
+                        (list[i] as myObj_1040).Move();
                     }
-
-                    if (doFillShapes)
-                    {
-                        // Tell the fragment shader to multiply existing instance opacity by 0.5:
-                        inst.SetColorA(-0.5f);
-                        inst.Draw(true);
-                    }
-
-                    // Tell the fragment shader to do nothing with the existing instance opacity:
-                    inst.SetColorA(0);
-                    inst.Draw(false);
                 }
 
                 if (Count < N)
@@ -232,6 +155,14 @@ namespace my
 
                 cnt++;
                 System.Threading.Thread.Sleep(renderDelay);
+
+                if (cnt == 3000)
+                {
+                    cnt = 0;
+
+                    colorPicker.reloadImage();
+                    tex.reloadImg(colorPicker.getImg());
+                }
             }
 
             return;
@@ -242,10 +173,11 @@ namespace my
         private void initShapes()
         {
             myPrimitive.init_ScrDimmer();
-            base.initShapes(shape, N, 0);
 
             grad = new myScreenGradient();
             grad.SetRandomColors(rand, 0.2f);
+
+            tex = new myTexRectangle(colorPicker.getImg());
 
             return;
         }
