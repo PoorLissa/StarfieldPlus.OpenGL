1c073d50a55e67aeb245e2bebc7081a91dbf9b1f
poorLissa <poortwistme@gmail.com>
2025-04-09 20:32:28 -0400

    ...

--- StarfieldPlus.OpenGL/myScrObjects/myObj_1280.cs
+++ StarfieldPlus.OpenGL/myScrObjects/myObj_1280.cs
@@ -17,13 +17,13 @@ namespace my
         public static int Priority => 10;
 		public static System.Type Type => typeof(myObj_1280);
 
-        private int yMax, yMin;
+        private int yMax, yMin, cnt;
         private float x, y, dx, dy;
-        private float sizeX, sizeY, A, R, G, B, angle = 0;
+        private float sizeX, sizeY, A, R, G, B;
 
-        private static int N = 0, shape = 0;
-        private static bool doFillShapes = false;
-        private static float dimAlpha = 0.05f;
+        private static int N = 0, spdMax = 1;
+        private static int opacityMode = 0, sizeMode = 0;
+        private static float dimAlpha = 0.05f, sizeYfactor = 1;
 
         private static myScreenGradient grad = null;
         private static Polygon4 p4 = null;
@@ -47,7 +47,7 @@ namespace my
             // Global unmutable constants
             {
                 N = rand.Next(10) + 10;
-                N = 100;
+                N = 100 + rand.Next(100);
             }
 
             initLocal();
@@ -61,6 +61,12 @@ namespace my
             doClearBuffer = myUtils.randomChance(rand, 1, 2);
             doClearBuffer = true;
 
+            sizeMode = rand.Next(2);
+            opacityMode = rand.Next(4);
+
+            spdMax = 1 + rand.Next(5);
+            sizeYfactor = 1.0f + myUtils.randFloat(rand) * (rand.Next(3) + 1);
+
             return;
         }
 
@@ -70,8 +76,12 @@ namespace my
         {
             height = 600;
 
-            string str = $"Obj = {Type}\n\n"                  +
-                            myUtils.strCountOf(list.Count, N) +
+            string str = $"Obj = {Type}\n\n"                               +
+                            myUtils.strCountOf(list.Count, N)              +
+                            $"opacityMode = {opacityMode}\n"               +
+                            $"sizeMode = {sizeMode}\n"                     +
+                            $"spdMax = {spdMax}\n"                         +
+                            $"sizeYfactor = {myUtils.fStr(sizeYfactor)}\n" +
                             $"file: {colorPicker.GetFileName()}"
                 ;
             return str;
@@ -89,14 +99,26 @@ namespace my
 
         protected override void generateNew()
         {
+            cnt = 10 + rand.Next(123);
+
             x = rand.Next(gl_Width);
             y = gl_Height + 100 + rand.Next(123);
 
             dx = 0;
-            dy = myUtils.randFloatClamped(rand, 0.25f) * (rand.Next(10) + 1) * -1.0f;
+            dy = myUtils.randFloatClamped(rand, 0.1f) * (rand.Next(spdMax) + 1) * -1.0f;
 
-            sizeX = 100;
-            sizeY = sizeX/2;
+            switch (sizeMode)
+            {
+                case 0:
+                    sizeX = 100;
+                    break;
+
+                case 1:
+                    sizeX = 50 + rand.Next(100);
+                    break;
+            }
+
+            sizeY = sizeX / sizeYfactor;
 
             yMax = gl_Height - rand.Next(333);
             yMin = rand.Next(333);
@@ -106,9 +128,24 @@ namespace my
             G = (float)rand.NextDouble();
             B = (float)rand.NextDouble();
 
-            R = G = B = 0.3f;
+            switch (opacityMode)
+            {
+                case 0:
+                case 1:
+                    A = 1;
+                    break;
 
-            //colorPicker.getColor(x, y, ref R, ref G, ref B);
+                case 2:
+                    if (id < N / 2)
+                        A = myUtils.randFloat(rand);
+                    break;
+
+                case 3:
+                    A = myUtils.randFloatClamped(rand, 0.1f);
+                    break;
+            }
+
+            colorPicker.getColor(rand.Next(gl_Width), rand.Next(gl_Height), ref R, ref G, ref B);
 
             return;
         }
@@ -117,18 +154,33 @@ namespace my
 
         protected override void Move()
         {
-            x += dx;
-            y += dy;
+            bool doFlip = false;
 
             if (y < yMin && dy < 0)
             {
-                dy *= -1;
+                doFlip = true;
             }
 
             if (y > yMax && dy > 0)
             {
-                dy *= -1;
+                doFlip = true;
             }
+
+            if (doFlip)
+            {
+                if (--cnt == 0)
+                {
+                    cnt = 10 + rand.Next(123);
+                    dy *= -1;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            x += dx;
+            y += dy;
 
             return;
         }
@@ -147,13 +199,19 @@ namespace my
             float y4 = y;
 
             p4.SetColor(R, G, B, A);
-            p4.Draw(x1, y1, x2, y2, x3, y3, x4, y4, !false);
+            p4.Draw(x1, y1, x2, y2, x3, y3, x4, y4, true);
+            p4.SetColor(0, 0, 0, 1);
+            p4.Draw(x1, y1, x2, y2, x3, y3, x4, y4, false);
 
             p4.SetColor(R * 1.1f, G * 1.1f, B * 1.1f, A);
-            p4.Draw(x1, y1, x3, y3, x1, gl_Height, x3, gl_Height, !false);
+            p4.Draw(x1, y1, x3, y3, x1, gl_Height, x3, gl_Height, true);
+            p4.SetColor(0, 0, 0, 1);
+            p4.Draw(x1, y1, x3, y3, x1, gl_Height, x3, gl_Height, false);
 
             p4.SetColor(R * 0.9f, G * 0.9f, B * 0.9f, A);
-            p4.Draw(x4, y4, x3, y3, x4, gl_Height, x3, gl_Height, !false);
+            p4.Draw(x4, y4, x3, y3, x4, gl_Height, x3, gl_Height, true);
+            p4.SetColor(0, 0, 0, 1);
+            p4.Draw(x4, y4, x3, y3, x4, gl_Height, x3, gl_Height, false);
 
             return;
         }
