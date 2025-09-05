9cb5234df3602fca83360bc23323290c7716f649
pm <pm@CM33L63.com>
2025-09-05 16:40:07 -0400

    ...

--- StarfieldPlus.OpenGL/ScreenSaver.cs
+++ StarfieldPlus.OpenGL/ScreenSaver.cs
@@ -279,7 +279,7 @@ public class ScreenSaver
 
         // Randomly pick one single object
         _obj = my.myObj_Prioritizer.GetRandomObject(doUsePriority   : true,
-                                                    doUseCustomType : false, my.myObj_1520.Type);
+                                                    doUseCustomType : !false, my.myObj_1530.Type);
         return;
     }
 
---
+++ StarfieldPlus.OpenGL/myScrObjects/myObj_1530.cs
@@ -0,0 +1,196 @@
+﻿using GLFW;
+using static OpenGL.GL;
+using System;
+using System.Collections.Generic;
+
+
+/*
+    - 
+*/
+
+
+namespace my
+{
+    public class myObj_1530 : myObject
+    {
+        // Priority
+        public static int Priority => 10;
+		public static System.Type Type => typeof(myObj_1530);
+
+        private int x, y, xTarget, yTarget, lifeCnt;
+
+        private static int N = 0, size = 50;
+
+        static myTexRectangle tex = null;
+
+        // ---------------------------------------------------------------------------------------------------------------
+
+        public myObj_1530()
+        {
+            if (id != uint.MaxValue)
+                generateNew();
+        }
+
+        // ---------------------------------------------------------------------------------------------------------------
+
+        // One-time global initialization
+        protected override void initGlobal()
+        {
+            colorPicker = new myColorPicker(gl_Width, gl_Height, mode: myColorPicker.colorMode.SNAPSHOT_OR_IMAGE);
+            list = new List<myObject>();
+
+            // Global unmutable constants
+            {
+                N = rand.Next(10) + 10;
+            }
+
+            initLocal();
+        }
+
+        // ---------------------------------------------------------------------------------------------------------------
+
+        // One-time local initialization
+        private void initLocal()
+        {
+            doClearBuffer = myUtils.randomChance(rand, 1, 2);
+
+            return;
+        }
+
+        // ---------------------------------------------------------------------------------------------------------------
+
+        protected override string CollectCurrentInfo(ref int width, ref int height)
+        {
+            height = 600;
+
+            string str = $"Obj = {Type}\n\n"                  +
+                            myUtils.strCountOf(list.Count, N) +
+                            $"file: {colorPicker.GetFileName()}"
+                ;
+            return str;
+        }
+
+        // ---------------------------------------------------------------------------------------------------------------
+
+        // 
+        protected override void setNextMode()
+        {
+            initLocal();
+        }
+
+        // ---------------------------------------------------------------------------------------------------------------
+
+        protected override void generateNew()
+        {
+            int gap = 3;
+
+            x = rand.Next(gl_Width);
+            y = rand.Next(gl_Height);
+
+            x -= x % (size + gap);
+            y -= y % (size + gap);
+
+            xTarget = x + myUtils.randomSign(rand) * rand.Next(size * 3);
+            yTarget = y + myUtils.randomSign(rand) * rand.Next(size * 3);
+
+            xTarget -= xTarget % (size + gap);
+            yTarget -= yTarget % (size + gap);
+
+            lifeCnt = 10 + rand.Next(11);
+
+            return;
+        }
+
+        // ---------------------------------------------------------------------------------------------------------------
+
+        protected override void Move()
+        {
+            if (--lifeCnt == 0)
+            {
+                generateNew();
+            }
+
+            return;
+        }
+
+        // ---------------------------------------------------------------------------------------------------------------
+
+        protected override void Show()
+        {
+            tex.setOpacity(1);
+            tex.Draw(x, y, size, size, xTarget, yTarget, size, size);
+        }
+
+        // ---------------------------------------------------------------------------------------------------------------
+
+        protected override void Process(Window window)
+        {
+            uint cnt = 0;
+            initShapes();
+
+            clearScreenSetup(doClearBuffer, 0.1f);
+
+            stopwatch = new StarfieldPlus.OpenGL.myUtils.myStopwatch(true);
+
+            while (!Glfw.WindowShouldClose(window))
+            {
+                int Count = list.Count;
+
+                processInput(window);
+
+                // Swap fore/back framebuffers, and poll for operating system events.
+                Glfw.SwapBuffers(window);
+                Glfw.PollEvents();
+
+                // Dim screen
+                {
+/*
+                    if (doClearBuffer)
+                    {
+                        glClear(GL_COLOR_BUFFER_BIT);
+                        grad.Draw();
+                    }
+                    else
+                    {
+                        dimScreen(dimAlpha);
+                    }
+*/
+                }
+
+                // Render Frame
+                {
+                    for (int i = 0; i != Count; i++)
+                    {
+                        var obj = list[i] as myObj_1530;
+
+                        obj.Show();
+                        obj.Move();
+                    }
+                }
+
+                if (Count < N)
+                {
+                    list.Add(new myObj_1530());
+                }
+
+                stopwatch.WaitAndRestart();
+                cnt++;
+            }
+
+            return;
+        }
+
+        // ---------------------------------------------------------------------------------------------------------------
+
+        private void initShapes()
+        {
+            myPrimitive.init_ScrDimmer();
+
+            tex = new myTexRectangle(colorPicker.getImg());
+
+            return;
+        }
+
+        // ---------------------------------------------------------------------------------------------------------------
+    }
+};
