dbf17545615e38e69b8e48520d6170f4b04d086f
pm <pm@CM33L63.com>
2025-10-31 11:51:36 -0400

    ...

--- StarfieldPlus.OpenGL/ScreenSaver.cs
+++ StarfieldPlus.OpenGL/ScreenSaver.cs
@@ -285,7 +285,7 @@ public class ScreenSaver
 
         // Randomly pick one single object
         _obj = my.myObj_Prioritizer.GetRandomObject(doUsePriority   : true,
-                                                    doUseCustomType : true, my.myObj_1540.Type);
+                                                    doUseCustomType : true, my.myObj_1545.Type);
         return;
     }
 
--- StarfieldPlus.OpenGL/myScrObjects/myObj_1545.cs
+++ StarfieldPlus.OpenGL/myScrObjects/myObj_1545.cs
@@ -12,27 +12,25 @@ using System.Collections.Generic;
 namespace my
 {
 #pragma warning disable 0162
-    public class myObj_1540 : myObject
+    public class myObj_1545 : myObject
     {
         // Priority
         public static int Priority => 10;
-        public static System.Type Type => typeof(myObj_1540);
+        public static System.Type Type => typeof(myObj_1545);
 
-        private int cnt, cnt2;
+        private int cnt, lifeCnt;
         private float x, y, dx, dy;
-        private float size, sizeBlur, A, R, G, B, angle = 0;
+        private float A, R, G, B;
 
-        private static int N = 0, n = 0, shape = 0, colorMode = 0;
-        private static bool doFillShapes = false, doDestroy = false, doDraw2ndLayer = false, doUse_dBlur = false;
-        private static float dBlur = 0;
+        private static int N = 0, n = 0, colorMode = 0;
+        private static bool doDestroy = false;
         private static float dimAlpha = 0.05f;
 
         private static myScreenGradient grad = null;
-        private static myFreeShader_001 shader = null;
 
         // ---------------------------------------------------------------------------------------------------------------
 
-        public myObj_1540()
+        public myObj_1545()
         {
             if (id != uint.MaxValue)
                 generateNew();
@@ -48,20 +46,8 @@ namespace my
 
             // Global unmutable constants
             {
-                doDraw2ndLayer = myUtils.randomBool(rand);
-                doUse_dBlur = myUtils.randomChance(rand, 1, 3);
-
                 n = 11;
-                N = 25000;
-                dBlur = 0.5f;
-
-                if (doDraw2ndLayer)
-                {
-                    N = 5000;
-                    dBlur = 0.9f;
-                }
-
-                shape = rand.Next(5);
+                N = 10000;
             }
 
             initLocal();
@@ -72,7 +58,6 @@ namespace my
         // One-time local initialization
         private void initLocal()
         {
-            doClearBuffer = myUtils.randomChance(rand, 1, 2);
             doClearBuffer = false;
             doDestroy = myUtils.randomBool(rand);
 
@@ -89,10 +74,9 @@ namespace my
 
             string str = $"Obj = {Type}\n\n"                       +
                             myUtils.strCountOf(list.Count, N)      +
+                            $"doClearBuffer = {doClearBuffer}\n"   +
                             $"colorMode = {colorMode}\n"           +
                             $"doDestroy = {doDestroy}\n"           +
-                            $"doDraw2ndLayer = {doDraw2ndLayer}\n" +
-                            $"doUse_dBlur = {doUse_dBlur}\n"       +
                             $"file: {colorPicker.GetFileName()}"
                 ;
             return str;
@@ -122,8 +106,6 @@ namespace my
 
                 A = 1;
                 R = G = B = 1;
-                size = 3;
-                sizeBlur = size + 0.01f;
                 cnt = 50 + rand.Next(111); // used a a dist
             }
             else
@@ -136,13 +118,10 @@ namespace my
                 dx = (0.1f + myUtils.randFloat(rand)) * myUtils.randomSign(rand);
                 dy = (0.1f + myUtils.randFloat(rand)) * myUtils.randomSign(rand);
 
-                size = 1;
-                sizeBlur = 0;
-
                 A = 0.15f + myUtils.randFloat(rand) * 0.1f;
                 colorPicker.getColor(x, y, ref R, ref G, ref B);
                 cnt = 333 + rand.Next(999);
-                cnt2 = 0;
+                lifeCnt = 33;
             }
 
             return;
@@ -176,7 +155,7 @@ namespace my
                     generateNew();
                 }
 
-                if (doDestroy && cnt2 > 33)
+                if (doDestroy && lifeCnt == 0)
                 {
                     generateNew();
                 }
@@ -196,7 +175,7 @@ namespace my
 
                 for (int i = n; i < Count; i++)
                 {
-                    var other = list[i] as myObj_1540;
+                    var other = list[i] as myObj_1545;
 
                     float dx = x - other.x;
                     float dy = y - other.y;
@@ -218,89 +197,24 @@ namespace my
                                 break;
                         }
 
-                        other.cnt2++;
-                        other.sizeBlur += dBlur;
+                        other.lifeCnt--;
 
-                        if (other.sizeBlur > 133)
-                            other.sizeBlur = 133;
-
-                        if (doDraw2ndLayer)
+                        for (int j = n; j < Count; j++)
                         {
-                            for (int j = n; j < Count; j++)
+                            var oth = list[j] as myObj_1545;
+
+                            dx = other.x - oth.x;
+                            dy = other.y - oth.y;
+
+                            dist = (float)Math.Sqrt(dx * dx + dy * dy);
+
+                            if (dist < 50)
                             {
-                                var oth = list[j] as myObj_1540;
-
-                                dx = other.x - oth.x;
-                                dy = other.y - oth.y;
-
-                                dist = (float)Math.Sqrt(dx * dx + dy * dy);
-
-                                if (dist < 50)
-                                {
-                                    myPrimitive._LineInst.setInstanceCoords(other.x, other.y, oth.x, oth.y);
-                                    myPrimitive._LineInst.setInstanceColor(1, 1, 1, 0.05f);
-                                }
+                                myPrimitive._LineInst.setInstanceCoords(other.x, other.y, oth.x, oth.y);
+                                myPrimitive._LineInst.setInstanceColor(1, 1, 1, 0.05f);
                             }
                         }
                     }
-                }
-            }
-
-            return;
-
-            float size2x = size * 2;
-
-            switch (shape)
-            {
-                // Instanced squares
-                case 0:
-                    myPrimitive._RectangleInst.setInstanceCoords(x - size, y - size, size2x, size2x);
-                    myPrimitive._RectangleInst.setInstanceColor(R, G, B, A);
-                    myPrimitive._RectangleInst.setInstanceAngle(angle);
-                    break;
-
-                // Instanced triangles
-                case 1:
-                    myPrimitive._TriangleInst.setInstanceCoords(x, y, size, angle);
-                    myPrimitive._TriangleInst.setInstanceColor(R, G, B, A);
-                    break;
-
-                // Instanced circles
-                case 2:
-                    myPrimitive._EllipseInst.setInstanceCoords(x, y, size2x, angle);
-                    myPrimitive._EllipseInst.setInstanceColor(R, G, B, A);
-                    break;
-
-                // Instanced pentagons
-                case 3:
-                    myPrimitive._PentagonInst.setInstanceCoords(x, y, size2x, angle);
-                    myPrimitive._PentagonInst.setInstanceColor(R, G, B, A);
-                    break;
-
-                // Instanced hexagons
-                case 4:
-                    myPrimitive._HexagonInst.setInstanceCoords(x, y, size2x, angle);
-                    myPrimitive._HexagonInst.setInstanceColor(R, G, B, A);
-                    break;
-            }
-
-            if (id < n)
-            {
-                float size4x = size * 4;
-
-                int off = 150 + (int)(size * 0.25f);
-                shader.SetColor(R, G, B, A * 0.25f);
-                shader.Draw(x, y, size4x, size4x, 0.02f, off);
-            }
-            else
-            {
-                if (doUse_dBlur && sizeBlur > 1.0f)
-                {
-                    int off = 150 + (int)(sizeBlur * 0.25f);
-                    shader.SetColor(R, G, B, A * 0.25f);
-                    shader.Draw(x, y, sizeBlur, sizeBlur, 0.02f, off);
-
-                    sizeBlur -= 0.1f;
                 }
             }
 
@@ -316,9 +230,9 @@ namespace my
 
             clearScreenSetup(doClearBuffer, 0.1f);
 
-            if (false)
+            if (!false)
                 while (list.Count < N)
-                    list.Add(new myObj_1540());
+                    list.Add(new myObj_1545());
 
             stopwatch = new StarfieldPlus.OpenGL.myUtils.myStopwatch(true);
 
@@ -347,34 +261,22 @@ namespace my
 
                 // Render Frame
                 {
-                    inst.ResetBuffer();
                     myPrimitive._LineInst.ResetBuffer();
 
                     for (int i = 0; i != Count; i++)
                     {
-                        var obj = list[i] as myObj_1540;
+                        var obj = list[i] as myObj_1545;
 
                         obj.Show();
                         obj.Move();
                     }
 
                     myPrimitive._LineInst.Draw();
-
-                    if (doFillShapes)
-                    {
-                        // Tell the fragment shader to multiply existing instance opacity by 0.5:
-                        inst.SetColorA(-0.5f);
-                        inst.Draw(true);
-                    }
-
-                    // Tell the fragment shader to do nothing with the existing instance opacity:
-                    inst.SetColorA(0);
-                    inst.Draw(false);
                 }
 
                 if (Count < N)
                 {
-                    list.Add(new myObj_1540());
+                    list.Add(new myObj_1545());
                 }
 
                 stopwatch.WaitAndRestart();
@@ -389,32 +291,16 @@ namespace my
         private void initShapes()
         {
             myPrimitive.init_ScrDimmer();
-            base.initShapes(shape, N, 0);
 
             grad = new myScreenGradient();
             grad.SetRandomColors(rand, 0.2f);
 
             myPrimitive.init_LineInst(n * 10000);
 
-            getShader();
-
             return;
         }
 
         // ---------------------------------------------------------------------------------------------------------------
-
-        // General shader selector
-        private void getShader()
-        {
-            string fHeader = "", fMain = "";
-
-            myFreeShader_001.getShader_000(ref fHeader, ref fMain, 0);
-
-            shader = new myFreeShader_001(fHeader, fMain);
-        }
-
-        // ---------------------------------------------------------------------------------------------------------------
-
     }
 #pragma warning restore 0162
 };
