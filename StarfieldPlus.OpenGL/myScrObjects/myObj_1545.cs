85bc85a978243446d77ee68dc968a575e0c21189
pm <pm@CM33L63.com>
2025-10-31 14:15:12 -0400

    ...

--- StarfieldPlus.OpenGL/myScrObjects/myObj_1545.cs
+++ StarfieldPlus.OpenGL/myScrObjects/myObj_1545.cs
@@ -2,6 +2,8 @@
 using static OpenGL.GL;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 
 /*
@@ -11,6 +13,108 @@ using System.Collections.Generic;
 
 namespace my
 {
+    public class myCellManager<Type>
+    {
+        public class myCellManager_Cell<Type>
+        {
+            public int x, y;
+            public Dictionary<uint, Type> items = null;
+
+            public myCellManager_Cell()
+            {
+                items = new Dictionary<uint, Type>();
+            }
+        }
+
+        public Dictionary<int, myCellManager_Cell<Type>> _dic = null;
+        private int _offset;
+
+        public int _cellSize;
+        public int _cellRowSize;
+        public int _minId, _maxId;
+
+        public myCellManager(int cellSize, int offset, int width, int height)
+        {
+            _cellSize = cellSize;
+            _offset = offset;
+
+            _dic = new Dictionary<int, myCellManager_Cell<Type>>();
+
+            int minx = -offset;
+            int maxx = width + offset;
+            int miny = -offset;
+            int maxy = height + offset;
+
+            _cellRowSize = 1 + (maxx - minx) / cellSize;
+
+            _minId = 99 * 99 * 99;
+            _maxId = -1;
+
+            for (int j = miny; j < maxy; j += cellSize)
+            {
+                for (int i = minx; i < maxx; i += cellSize)
+                {
+                    int id = i / cellSize + (j / cellSize) * _cellRowSize;
+
+                    Debug.Assert(!_dic.ContainsKey(id));
+
+                    if (id > _maxId)
+                        _maxId = id;
+
+                    if (id < _minId)
+                        _minId = id;
+
+                    _dic[id] = new myCellManager_Cell<Type>();
+
+                    _dic[id].x = i / cellSize;
+                    _dic[id].y = j / cellSize;
+                }
+            }
+        }
+
+        public int GetCellId(float x, float y)
+        {
+            return (int)x / _cellSize + ((int)y / _cellSize) * _cellRowSize;
+        }
+
+        public int Add(float x, float y, uint id, Type item)
+        {
+            int cellId = GetCellId(x, y);
+
+            _dic[cellId].items.Add(id, item);
+
+            return cellId;
+        }
+
+        public int Move(float x, float y, uint id, int oldCellId, Type item)
+        {
+            int newCellId = GetCellId(x, y);
+
+            if (oldCellId != newCellId)
+            {
+                if (oldCellId >= _minId && oldCellId <= _maxId)
+                {
+                    _dic[oldCellId].items.Remove(id);
+                }
+
+                if (newCellId >= _minId && newCellId <= _maxId)
+                {
+                    _dic[newCellId].items.Add(id, item);
+                }
+
+                return newCellId;
+            }
+
+            return oldCellId;
+        }
+    }
+
+
+
+
+
+
+
 #pragma warning disable 0162
     public class myObj_1545 : myObject
     {
@@ -18,15 +122,17 @@ namespace my
         public static int Priority => 10;
         public static System.Type Type => typeof(myObj_1545);
 
-        private int cnt, lifeCnt;
+        private int cnt, lifeCnt, cellId = -1000;
         private float x, y, dx, dy;
         private float A, R, G, B;
 
         private static int N = 0, n = 0, colorMode = 0;
+        private static int cellSize = 100;
         private static bool doDestroy = false;
         private static float dimAlpha = 0.05f;
 
         private static myScreenGradient grad = null;
+        private static myCellManager<myObj_1545> cellManager = null;
 
         // ---------------------------------------------------------------------------------------------------------------
 
@@ -44,9 +150,13 @@ namespace my
             colorPicker = new myColorPicker(gl_Width, gl_Height);
             list = new List<myObject>();
 
+            cellManager = new myCellManager<myObj_1545>(100, 0, gl_Width, gl_Height);
+
             // Global unmutable constants
             {
                 n = 11;
+                N = 5000;
+                N = 300000;
                 N = 10000;
             }
 
@@ -124,6 +234,8 @@ namespace my
                 lifeCnt = 33;
             }
 
+            cellId = cellManager.Move(x, y, id, cellId, this);
+
             return;
         }
 
@@ -159,16 +271,130 @@ namespace my
                 {
                     generateNew();
                 }
-
             }
+
+            cellId = cellManager.Move(x, y, id, cellId, this);
 
             return;
         }
 
         // ---------------------------------------------------------------------------------------------------------------
 
+        private void Show_2(myObj_1545 item)
+        {
+            if (item.id >= n)
+            {
+                int maxDist = 25;
+
+                float dx, dy, dist2;
+
+                int rayDist = maxDist / cellManager._cellSize;
+
+                int min = -rayDist - 1;
+                int max = +rayDist + 2;
+
+                int cnt2 = maxDist * maxDist;
+
+                for (int i = min; i < max; i++)
+                {
+                    int cell_i = item.cellId + cellManager._cellRowSize * i;
+
+                    for (int j = min; j < max; j++)
+                    {
+                        int cell = cell_i + j;
+
+                        if (cell >= cellManager._minId && cell <= cellManager._maxId)
+                        {
+                            foreach (var other in cellManager._dic[cell].items)
+                            {
+                                // Prevent drawing the same line more than once
+                                //if (id < other.Value.id)
+                                {
+                                    dx = item.x - other.Value.x;
+                                    dy = item.y - other.Value.y;
+
+                                    dist2 = dx * dx + dy * dy;
+
+                                    // Other particle is within reach of the first one
+                                    if (dist2 < cnt2)
+                                    {
+                                        myPrimitive._LineInst.setInstanceCoords(other.Value.x, other.Value.y, item.x, item.y);
+                                        myPrimitive._LineInst.setInstanceColor(1, 1, 1, 0.05f);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return;
+        }
+
         protected override void Show()
         {
+#if true
+
+            if (id < n)
+            {
+                float dx, dy, dist2;
+
+                int rayDist = cnt / cellManager._cellSize;
+
+                int min = -rayDist - 1;
+                int max = +rayDist + 2;
+
+                int cnt2 = cnt * cnt;
+
+                for (int i = min; i < max; i++)
+                {
+                    int cell_i = cellId + cellManager._cellRowSize * i;
+
+                    for (int j = min; j < max; j++)
+                    {
+                        int cell = cell_i + j;
+
+                        if (cell >= cellManager._minId && cell <= cellManager._maxId)
+                        {
+                            foreach (var other in cellManager._dic[cell].items)
+                            {
+                                // Prevent drawing the same line more than once
+                                if (id < other.Value.id)
+                                {
+                                    dx = x - other.Value.x;
+                                    dy = y - other.Value.y;
+
+                                    dist2 = dx * dx + dy * dy;
+
+                                    // Other particle is within reach of the first one
+                                    if (dist2 < cnt2)
+                                    {
+                                        myPrimitive._LineInst.setInstanceCoords(x, y, other.Value.x, other.Value.y);
+
+                                        myPrimitive._LineInst.setInstanceColor(1, 0, 0, 0.1f);
+
+/*
+                                        switch (colorMode)
+                                        {
+                                            case 0:
+                                                myPrimitive._LineInst.setInstanceColor(1, 1, 1, 0.05f);
+                                                break;
+
+                                            case 1:
+                                                myPrimitive._LineInst.setInstanceColor(other.Value.R, other.Value.G, other.Value.B, 0.05f);
+                                                break;
+                                        }
+*/
+                                        Show_2(other.Value);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+#else
             if (id < n)
             {
                 int Count = list.Count;
@@ -217,7 +443,7 @@ namespace my
                     }
                 }
             }
-
+#endif
             return;
         }
 
@@ -302,5 +528,4 @@ namespace my
 
         // ---------------------------------------------------------------------------------------------------------------
     }
-#pragma warning restore 0162
 };
